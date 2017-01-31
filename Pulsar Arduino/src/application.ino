88256e4675096310121e9494dfe425fc96a813fb
nsobczak <nicolas.sobczak@orange.fr>
2017-01-31 15:41:16 +0100

    Pulsar Arduino: ajout de l'affichage des valeurs à l'écran

--- Pulsar Arduino/src/application.ino
+++ Pulsar Arduino/src/application.ino
@@ -2,30 +2,70 @@
   * Pulsar Arduino
   */
 //_________________________________________________________________
+#include <LiquidCrystal.h>
+// initialize the library with the numbers of the interface pins for the LCD
+# define rs  15
+# define rw  0
+# define en  14
+# define d4  4
+# define d5  5
+# define d6  6
+# define d7  7
+LiquidCrystal lcd(rs, rw, en, d4, d5, d6, d7);
 
+# define BUTTON1 8
+# define BUTTON2 9
 # define POTENTIOMETER_PIN 3
 
 int value;
 float reading;
+int delayBetweenValue;
 
 
 //_________________________________________________________________
 
+int changeDelayBetweenValue(int delay) {
+  if (digitalRead(BUTTON1)==LOW){
+    delay += 10;
+  }
+  if (digitalRead(BUTTON2)==LOW){
+    delay -= 10;
+  }
+  return delay;
+}
+
+
 void readPotentiometerValue(int delayBetweenValue){
   reading = analogRead(POTENTIOMETER_PIN);
-  // Serial.print("\t reading : \t");
-  // Serial.println(reading);
   value = reading*401/1024 ;  //analogRead(PIN)*RANGE/1024.0
-  //Serial.print("\t value : \t");
   Serial.println(value);
+
+  lcd.setCursor(8, 0);
+  lcd.print(value);
+  lcd.setCursor(8, 1);
+  lcd.print(delayBetweenValue);
+
   delay(delayBetweenValue);
 }
 
+
 void setup() {
-  pinMode(POTENTIOMETER_PIN,INPUT);
+  lcd.begin(16, 2);
+  lcd.clear();
+  pinMode(BUTTON1, INPUT);
+  pinMode(BUTTON2, INPUT);
+  lcd.setCursor(0, 0);
+  lcd.print("value : ");
+  lcd.setCursor(0, 1);
+  lcd.print("delay : ");
+
+  delayBetweenValue = 500;
+  pinMode(POTENTIOMETER_PIN, INPUT);
   Serial.begin(9600);
 }
 
+
 void loop() {
-  readPotentiometerValue(500);
+  delayBetweenValue = changeDelayBetweenValue(delayBetweenValue);
+  readPotentiometerValue(delayBetweenValue);
 }
