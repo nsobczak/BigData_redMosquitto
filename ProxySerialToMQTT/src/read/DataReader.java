d5c0fbf68e0e5a36a366bc57fafbc90ec508d6a8
nsobczak <nicolas.sobczak@orange.fr>
2017-02-04 15:51:04 +0100

    ProxyToMQTT: Mise en forme du code

--- ProxySerialToMQTT/src/read/DataReader.java
+++ ProxySerialToMQTT/src/read/DataReader.java
@@ -3,6 +3,7 @@ package read;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+
 import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
 import gnu.io.SerialPortEvent;
@@ -13,58 +14,85 @@ import send.DataSender;
 
 import java.util.Enumeration;
 
+
+//______________________________________________________________________
+
 /**
- * Created by vvinc_000 on 31/01/2017.
+ * Created by ISEN on 31/01/2017.
+ * DataReader : reading of data received on the serial port from the arduino
  */
-public class DataReader implements SerialPortEventListener {
+public class DataReader implements SerialPortEventListener
+{
     SerialPort serialPort;
-    /** The port we're normally going to use. */
+
+    /**
+     * The port we're normally going to use.
+     */
     private static final String PORT_NAMES[] = {
             "/dev/tty.usbserial-A9007UX1", // Mac OS X
             "/dev/ttyACM0", // Raspberry Pi
             "/dev/ttyUSB0", // Linux
-            "COM1","COM2",
-            "COM3","COM4",
-            "COM5","COM6",
-            "COM7","COM8" // Windows
+            "COM1", "COM2",
+            "COM3", "COM4",
+            "COM5", "COM6",
+            "COM7", "COM8" // Windows
     };
+
     /**
      * A BufferedReader which will be fed by a InputStreamReader
      * converting the bytes into characters
      * making the displayed results codepage independent
      */
     private BufferedReader input;
-    /** The output stream to the port */
+
+    /**
+     * The output stream to the port
+     */
     private OutputStream output;
-    /** Milliseconds to block while waiting for port open */
+
+    /**
+     * Milliseconds to block while waiting for port open
+     */
     private static final int TIME_OUT = 2000;
-    /** Default bits per second for COM port. */
+
+    /**
+     * Default bits per second for COM port.
+     */
     private static final int DATA_RATE = 9600;
 
-    public void initialize() {
+
+    //______________________________________________________________________
+
+    public void initialize()
+    {
         // the next line is for Raspberry Pi and
         // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
-//        System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
+        // System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
 
         CommPortIdentifier portId = null;
         Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
 
         //First, Find an instance of serial port as set in PORT_NAMES.
-        while (portEnum.hasMoreElements()) {
+        while (portEnum.hasMoreElements())
+        {
             CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
-            for (String portName : PORT_NAMES) {
-                if (currPortId.getName().equals(portName)) {
+            for (String portName : PORT_NAMES)
+            {
+                if (currPortId.getName().equals(portName))
+                {
                     portId = currPortId;
                     break;
                 }
             }
         }
-        if (portId == null) {
+        if (portId == null)
+        {
             System.out.println("Could not find COM port.");
             return;
         }
 
-        try {
+        try
+        {
             // open serial port, and use class name for the appName.
             serialPort = (SerialPort) portId.open(this.getClass().getName(),
                     TIME_OUT);
@@ -82,52 +110,73 @@ public class DataReader implements SerialPortEventListener {
             // add event listeners
             serialPort.addEventListener(this);
             serialPort.notifyOnDataAvailable(true);
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             System.err.println(e.toString());
         }
     }
+
 
     /**
      * This should be called when you stop using the port.
      * This will prevent port locking on platforms like Linux.
      */
-    public synchronized void close() {
-        if (serialPort != null) {
+    public synchronized void close()
+    {
+        if (serialPort != null)
+        {
             serialPort.removeEventListener();
             serialPort.close();
         }
     }
 
+
     /**
      * Handle an event on the serial port. Read the data and print it.
+     * @param oEvent
      */
-    public synchronized void serialEvent(SerialPortEvent oEvent) {
-        if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
-            try {
-                String inputLine=input.readLine();
-                System.out.println("value : "+inputLine);
+    public synchronized void serialEvent(SerialPortEvent oEvent)
+    {
+        if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE)
+        {
+            try
+            {
+                String inputLine = input.readLine();
+                System.out.println("value : " + inputLine);
                 DataSender.SendValues(inputLine);
-            } catch (Exception e) {
+            } catch (Exception e)
+            {
                 System.err.println(e.toString());
             }
         }
         // Ignore all the other eventTypes, but you should consider the other ones.
     }
 
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws Exception
+    {
 
         MqttClient sampleClient = DataSender.SendValues("Launch Test");
         DataReader main = new DataReader();
         main.initialize();
-        Thread thread=new Thread() {
-            public void run() {
+        Thread thread = new Thread()
+        {
+            public void run()
+            {
                 //the following line will keep this send alive for 1000 seconds,
                 //waiting for events to occur and responding to them (printing incoming messages to console).
-                try {Thread.sleep(1000000);} catch (InterruptedException ie) {}
+                try
+                {
+                    Thread.sleep(1000000);
+                } catch (InterruptedException ie)
+                {
+                }
             }
         };
         thread.start();
         System.out.println("Started");
-//        DataSender.stopSender(sampleClient);
+        //DataSender.stopSender(sampleClient);
     }
+
+
 }
